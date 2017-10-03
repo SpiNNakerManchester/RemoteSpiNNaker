a34528d224eb3989fcb06ac72c08fa63f1dd0ee6
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-10-03 11:44:43 +0100

    Fixes to documentation generation

--- RemoteSpiNNaker/RemoteSpiNNakerJobProcessManager/src/main/java/uk/ac/manchester/cs/spinnaker/jobprocess/JobProcessFactory.java
+++ RemoteSpiNNaker/RemoteSpiNNakerJobProcessManager/src/main/java/uk/ac/manchester/cs/spinnaker/jobprocess/JobProcessFactory.java
@@ -12,96 +12,98 @@ import uk.ac.manchester.cs.spinnaker.job.JobParameters;
  * {@link JobParameters} instance
  */
 public class JobProcessFactory {
-    private final ThreadGroup threadGroup;
+	private final ThreadGroup threadGroup;
 
-    public JobProcessFactory(final ThreadGroup threadGroup) {
-        this.threadGroup = threadGroup;
-    }
+	public JobProcessFactory(ThreadGroup threadGroup) {
+		this.threadGroup = threadGroup;
+	}
 
-    public JobProcessFactory(final String threadGroupName) {
-        this(new ThreadGroup(threadGroupName));
-    }
+	public JobProcessFactory(String threadGroupName) {
+		this(new ThreadGroup(threadGroupName));
+	}
 
-    /**
-     * A map between parameter types and process types. Note that the type is
-     * guaranteed by the {@link #addMapping(Class,Class)} method, which is the
-     * only place that this map should be modified.
-     */
-    private final Map<Class<? extends JobParameters>,
-            Class<? extends JobProcess<? extends JobParameters>>> typeMap =
-                    new HashMap<>();
+	/**
+	 * A map between parameter types and process types. Note that the type is
+	 * guaranteed by the {@link #addMapping(Class,Class)} method, which is the
+	 * only place that this map should be modified.
+	 */
+	private final Map<Class<? extends JobParameters>,
+			Class<? extends JobProcess<? extends JobParameters>>> typeMap =
+		new HashMap<>();
 
-    /**
-     * Adds a new type mapping.
-     *
-     * @param parameterType
-     *            The job parameter type
-     * @param processType
-     *            The job process type
-     */
-    public <P extends JobParameters> void addMapping(
-            final Class<P> parameterType,
-            final Class<? extends JobProcess<P>> processType) {
-        typeMap.put(parameterType, processType);
-    }
+	/**
+	 * Adds a new type mapping.
+	 *
+	 * @param <ParamType>
+	 *            The type of parameter. Inferred from the
+	 *            <tt>parameterType</tt>.
+	 * @param parameterType
+	 *            The job parameter type
+	 * @param processType
+	 *            The job process type
+	 */
+	public <ParamType extends JobParameters> void addMapping(
+			Class<ParamType> parameterType,
+			Class<? extends JobProcess<ParamType>> processType) {
+		typeMap.put(parameterType, processType);
+	}
 
-    public Collection<Class<? extends JobParameters>> getParameterTypes() {
-        return typeMap.keySet();
-    }
+	public Collection<Class<? extends JobParameters>> getParameterTypes() {
+		return typeMap.keySet();
+	}
 
-    /**
-     * Creates a {@link JobProcess} given a {@link JobParameters} instance.
-     *
-     * @param parameters
-     *            The parameters of the job
-     * @return A JobProcess matching the parameters
-     * @throws IllegalAccessException
-     *             If there is an error creating the class
-     * @throws InstantiationException
-     *             If there is an error creating the class
-     */
-    public <P extends JobParameters> JobProcess<P>
-            createProcess(final P parameters)
-                    throws InstantiationException, IllegalAccessException {
-        /*
-         * We know that this is of the correct type, because the addMapping
-         * method will only allow the correct type mapping in
-         */
-        @SuppressWarnings("unchecked")
-        final Class<JobProcess<P>> processType =
-                (Class<JobProcess<P>>) typeMap.get(parameters.getClass());
+	/**
+	 * Creates a {@link JobProcess} given a {@link JobParameters} instance.
+	 *
+	 * @param parameters
+	 *            The parameters of the job
+	 * @return A JobProcess matching the parameters
+	 * @throws IllegalAccessException
+	 *             If there is an error creating the class
+	 * @throws InstantiationException
+	 *             If there is an error creating the class
+	 */
+	public <P extends JobParameters> JobProcess<P> createProcess(P parameters)
+			throws InstantiationException, IllegalAccessException {
+		/*
+		 * We know that this is of the correct type, because the addMapping
+		 * method will only allow the correct type mapping in
+		 */
+		@SuppressWarnings("unchecked")
+		final Class<JobProcess<P>> processType = (Class<JobProcess<P>>) typeMap
+				.get(parameters.getClass());
 
-        final JobProcess<P> process = processType.newInstance();
+		final JobProcess<P> process = processType.newInstance();
 
-        // Magically set the thread group if there is one
-        setField(process, "threadGroup", threadGroup);
+		// Magically set the thread group if there is one
+		setField(process, "threadGroup", threadGroup);
 
-        return process;
-    }
+		return process;
+	}
 
-    @SuppressWarnings("unused")
-    private static void setField(final Class<?> clazz, final String fieldName,
-            final Object value) {
-        try {
-            final Field threadGroupField = clazz.getDeclaredField(fieldName);
-            threadGroupField.setAccessible(true);
-            threadGroupField.set(null, value);
-        } catch (NoSuchFieldException | SecurityException
-                | IllegalArgumentException | IllegalAccessException e) {
-            // Treat any exception as just a simple refusal to set the field.
-        }
-    }
+	@SuppressWarnings("unused")
+	private static void setField(Class<?> clazz, String fieldName,
+			Object value) {
+		try {
+			Field threadGroupField = clazz.getDeclaredField(fieldName);
+			threadGroupField.setAccessible(true);
+			threadGroupField.set(null, value);
+		} catch (NoSuchFieldException | SecurityException
+				| IllegalArgumentException | IllegalAccessException e) {
+			// Treat any exception as just a simple refusal to set the field.
+		}
+	}
 
-    private static void setField(final Object instance, final String fieldName,
-            final Object value) {
-        try {
-            final Field threadGroupField =
-                    instance.getClass().getDeclaredField(fieldName);
-            threadGroupField.setAccessible(true);
-            threadGroupField.set(instance, value);
-        } catch (NoSuchFieldException | SecurityException
-                | IllegalArgumentException | IllegalAccessException e) {
-            // Treat any exception as just a simple refusal to set the field.
-        }
-    }
+	private static void setField(Object instance, String fieldName,
+			Object value) {
+		try {
+			Field threadGroupField = instance.getClass()
+					.getDeclaredField(fieldName);
+			threadGroupField.setAccessible(true);
+			threadGroupField.set(instance, value);
+		} catch (NoSuchFieldException | SecurityException
+				| IllegalArgumentException | IllegalAccessException e) {
+			// Treat any exception as just a simple refusal to set the field.
+		}
+	}
 }
--- RemoteSpiNNaker/RemoteSpiNNakerJobProcessManager/src/main/java/uk/ac/manchester/cs/spinnaker/utils/FileDownloader.java
+++ RemoteSpiNNaker/RemoteSpiNNakerJobProcessManager/src/main/java/uk/ac/manchester/cs/spinnaker/utils/FileDownloader.java
@@ -8,11 +8,10 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Map;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
+import java.util.Map;
+
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -23,137 +22,135 @@ import javax.net.ssl.X509TrustManager;
 import org.jboss.resteasy.util.ParameterParser;
 
 public class FileDownloader {
-    private static String getFileName(final String contentDisposition) {
-        if (contentDisposition != null) {
-            final String cdl = contentDisposition.toLowerCase();
-            if (cdl.startsWith("form-data") || cdl.startsWith("attachment")) {
-                final ParameterParser parser = new ParameterParser();
-                parser.setLowerCaseNames(true);
-                final Map<String, String> params =
-                        parser.parse(contentDisposition, ';');
-                if (params.containsKey("filename")) {
-                    return params.get("filename").trim();
-                }
-            }
-        }
-        return null;
-    }
+	private static String getFileName(String contentDisposition) {
+		if (contentDisposition != null) {
+			String cdl = contentDisposition.toLowerCase();
+			if (cdl.startsWith("form-data") || cdl.startsWith("attachment")) {
+				ParameterParser parser = new ParameterParser();
+				parser.setLowerCaseNames(true);
+				Map<String, String> params = parser.parse(contentDisposition,
+						';');
+				if (params.containsKey("filename")) {
+					return params.get("filename").trim();
+				}
+			}
+		}
+		return null;
+	}
 
-    /**
-     * Downloads a file from a URL.
-     *
-     * @param url
-     *            The url to download the file from
-     * @param workingDirectory
-     *            The directory to output the file to
-     * @param defaultFilename
-     *            The name of the file to use if none can be worked out from the
-     *            url or headers, or <tt>null</tt> to use a generated name
-     * @return The file downloaded
-     * @throws IOException
-     * @throws NoSuchAlgorithmException
-     * @throws KeyManagementException
-     */
-    public static File downloadFile(final URL url, final File workingDirectory,
-            final String defaultFilename) throws IOException {
-        requireNonNull(workingDirectory);
+	/**
+	 * Downloads a file from a URL.
+	 *
+	 * @param url
+	 *            The url to download the file from
+	 * @param workingDirectory
+	 *            The directory to output the file to
+	 * @param defaultFilename
+	 *            The name of the file to use if none can be worked out from the
+	 *            url or headers, or <tt>null</tt> to use a generated name
+	 * @return The file downloaded
+	 * @throws IOException
+	 *             If anything goes wrong.
+	 */
+	public static File downloadFile(URL url, File workingDirectory,
+			String defaultFilename) throws IOException {
+		requireNonNull(workingDirectory);
 
-        // Open a connection
-        final URLConnection urlConnection =
-                requireNonNull(url).openConnection();
-        urlConnection.setDoInput(true);
+		// Open a connection
+		URLConnection urlConnection = requireNonNull(url).openConnection();
+		urlConnection.setDoInput(true);
 
-        if (urlConnection instanceof HttpsURLConnection) {
-            initVeryTrustingSSLContext((HttpsURLConnection) urlConnection);
-        }
+		if (urlConnection instanceof HttpsURLConnection) {
+			initVeryTrustingSSLContext((HttpsURLConnection) urlConnection);
+		}
 
-        // Work out the output filename
-        final File output = getTargetFile(url, workingDirectory,
-                defaultFilename, urlConnection);
+		// Work out the output filename
+		File output = getTargetFile(url, workingDirectory, defaultFilename,
+				urlConnection);
 
-        // Write the file
-        copy(urlConnection.getInputStream(), output.toPath());
+		// Write the file
+		copy(urlConnection.getInputStream(), output.toPath());
 
-        return output;
-    }
+		return output;
+	}
 
-    /**
-     * Sets the given connection to trust any host for the purposes of HTTPS.
-     * This is wildly unsafe.
-     *
-     * @param connection
-     *            The connection to configure.
-     * @throws IOException
+	/**
+	 * Sets the given connection to trust any host for the purposes of HTTPS.
+	 * This is wildly unsafe.
+	 *
+	 * @param connection
+	 *            The connection to configure.
+	 * @throws IOException
+	 *             If anything goes wrong.
+	 */
+	private static void initVeryTrustingSSLContext(
+			HttpsURLConnection connection) throws IOException {
+		// Set up to trust everyone
+		try {
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, new TrustManager[] { new X509TrustManager() {
+				@Override
+				public X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+
+				@Override
+				public void checkClientTrusted(X509Certificate[] certs,
+						String authType) {
+				}
+
+				@Override
+				public void checkServerTrusted(X509Certificate[] certs,
+						String authType) {
+				}
+			} }, new SecureRandom());
+
+			connection.setSSLSocketFactory(sc.getSocketFactory());
+			connection.setHostnameVerifier(new HostnameVerifier() {
+				@Override
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			});
+		} catch (Exception e) {
+			throw new IOException("Error processing HTTPS request", e);
+		}
+	}
+
+	private static File getTargetFile(URL url, File workingDirectory,
+			String defaultFilename, URLConnection urlConnection)
+			throws IOException {
+		String filename = getFileName(
+				urlConnection.getHeaderField("Content-Disposition"));
+		if (filename != null) {
+			return new File(workingDirectory, filename);
+		}
+		if (defaultFilename != null) {
+			return new File(workingDirectory, defaultFilename);
+		}
+		String path = url.getPath();
+		if (path.isEmpty()) {
+			return createTempFile("download", "file", workingDirectory);
+		}
+		return new File(workingDirectory, new File(path).getName());
+	}
+
+	/**
+	 * Downloads a file from a URL.
+	 *
+	 * @param url
+	 *            The url to download the file from
+	 * @param workingDirectory
+	 *            The directory to output the file to
+	 * @param defaultFilename
+	 *            The name of the file to use if none can be worked out from the
+	 *            url or headers, or <tt>null</tt> to use a generated name
+	 * @return The file downloaded
+	 * @throws IOException
      *             If anything goes wrong.
-     */
-    private static void initVeryTrustingSSLContext(
-            HttpsURLConnection connection) throws IOException {
-        // Set up to trust everyone
-        try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, new TrustManager[]{new X509TrustManager() {
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-
-                @Override
-                public void checkClientTrusted(X509Certificate[] certs,
-                        String authType) {
-                }
-
-                @Override
-                public void checkServerTrusted(X509Certificate[] certs,
-                        String authType) {
-                }
-            }}, new SecureRandom());
-
-            connection.setSSLSocketFactory(sc.getSocketFactory());
-            connection.setHostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-        } catch (Exception e) {
-            throw new IOException("Error processing HTTPS request", e);
-        }
-    }
-
-    private static File getTargetFile(final URL url,
-            final File workingDirectory, final String defaultFilename,
-            final URLConnection urlConnection) throws IOException {
-        final String filename = getFileName(
-                urlConnection.getHeaderField("Content-Disposition"));
-        if (filename != null) {
-            return new File(workingDirectory, filename);
-        }
-        if (defaultFilename != null) {
-            return new File(workingDirectory, defaultFilename);
-        }
-        final String path = url.getPath();
-        if (path.isEmpty()) {
-            return createTempFile("download", "file", workingDirectory);
-        }
-        return new File(workingDirectory, new File(path).getName());
-    }
-
-    /**
-     * Downloads a file from a URL.
-     *
-     * @param url
-     *            The url to download the file from
-     * @param workingDirectory
-     *            The directory to output the file to
-     * @param defaultFilename
-     *            The name of the file to use if none can be worked out from the
-     *            url or headers, or <tt>null</tt> to use a generated name
-     * @return The file downloaded
-     * @throws IOException
-     */
-    public static File downloadFile(final String url,
-            final File workingDirectory, final String defaultFilename)
-            throws IOException {
-        return downloadFile(new URL(url), workingDirectory, defaultFilename);
-    }
+	 */
+	public static File downloadFile(String url, File workingDirectory,
+			String defaultFilename) throws IOException {
+		return downloadFile(new URL(url), workingDirectory, defaultFilename);
+	}
 }
