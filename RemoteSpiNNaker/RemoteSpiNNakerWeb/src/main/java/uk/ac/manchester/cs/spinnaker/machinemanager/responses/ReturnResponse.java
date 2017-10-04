71830419b3184eb19d35d3cdea6bade864456ea0
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-10-04 14:05:15 +0100

    Fixing the javadoc

--- RemoteSpiNNaker/RemoteSpiNNakerWeb/src/main/java/uk/ac/manchester/cs/spinnaker/machinemanager/responses/ReturnResponse.java
+++ RemoteSpiNNaker/RemoteSpiNNakerWeb/src/main/java/uk/ac/manchester/cs/spinnaker/machinemanager/responses/ReturnResponse.java
@@ -3,6 +3,9 @@ package uk.ac.manchester.cs.spinnaker.machinemanager.responses;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.databind.JsonNode;
 
+/**
+ * A response that is the result of a request.
+ */
 public class ReturnResponse implements Response {
 	private String returnValue;
 
