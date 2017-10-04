70a336ac28c7ec199505a94b339811eb8e4b2c6d
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-10-04 14:36:47 +0100

    Fixing the javadoc

--- RemoteSpiNNaker/RemoteSpiNNakerWeb/src/main/java/uk/ac/manchester/cs/spinnaker/nmpi/NMPIQueueListener.java
+++ RemoteSpiNNaker/RemoteSpiNNakerWeb/src/main/java/uk/ac/manchester/cs/spinnaker/nmpi/NMPIQueueListener.java
@@ -8,5 +8,13 @@ import uk.ac.manchester.cs.spinnaker.job.nmpi.Job;
  * An interface for things that listen for new jobs.
  */
 public interface NMPIQueueListener {
+	/**
+	 * Adds a job to the listener.
+	 *
+	 * @param job
+	 *            The job to add.
+	 * @throws IOException
+	 *             If anything goes wrong.
+	 */
 	void addJob(Job job) throws IOException;
 }
