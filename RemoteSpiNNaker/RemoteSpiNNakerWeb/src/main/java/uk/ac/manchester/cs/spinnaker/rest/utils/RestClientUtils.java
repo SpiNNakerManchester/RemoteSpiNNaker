e6e559804c5e08ae96d29dc842a96495de118748
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-08-06 08:24:11 +0100

    Simplify connectivity and actually use timeouts

--- RemoteSpiNNaker/RemoteSpiNNakerWeb/src/main/java/uk/ac/manchester/cs/spinnaker/rest/utils/RestClientUtils.java
+++ RemoteSpiNNaker/RemoteSpiNNakerWeb/src/main/java/uk/ac/manchester/cs/spinnaker/rest/utils/RestClientUtils.java
@@ -16,46 +16,26 @@
  */
 package uk.ac.manchester.cs.spinnaker.rest.utils;
 
-import static org.apache.http.auth.AUTH.PROXY_AUTH_RESP;
 import static org.apache.http.auth.AUTH.WWW_AUTH_RESP;
-import static org.apache.http.client.protocol.HttpClientContext.AUTH_CACHE;
-import static org.apache.http.client.protocol.HttpClientContext.CREDS_PROVIDER;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.io.IOException;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.http.Header;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
-import org.apache.http.auth.AuthScheme;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.AuthenticationException;
-import org.apache.http.auth.Credentials;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.AuthCache;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.auth.RFC2617Scheme;
-import org.apache.http.impl.client.BasicAuthCache;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EncodingUtils;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
-import org.jboss.resteasy.client.jaxrs.engines.HttpContextProvider;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder.HostnameVerificationPolicy;
 import org.slf4j.Logger;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
@@ -105,39 +85,22 @@ public abstract class RestClientUtils {
      *
      * @param url
      *            What this client talks to.
-     * @param credentials
-     *            What this client will authenticate with.
-     * @param authScheme
-     *            The authentication scheme.
      * @return the client
      */
-    protected static ResteasyClient createRestClient(final URL url,
-            final Credentials credentials, final AuthScheme authScheme) {
+    protected static ResteasyClient createRestClient(final URL url) {
         try {
-            final HttpContextProvider localContext = getConnectionContext(url,
-                    credentials, authScheme);
-
-            // Set up the connection
-            final PoolingHttpClientConnectionManager cm =
-                    new PoolingHttpClientConnectionManager();
-            cm.setMaxTotal(MAX_CONNECTIONS);
-            cm.setDefaultMaxPerRoute(MAX_CONNECTIONS_PER_ROUTE);
             SSLContextBuilder builder = new SSLContextBuilder();
             builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
-            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
-                    builder.build(), NoopHostnameVerifier.INSTANCE);
-            HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
-            httpClientBuilder.setConnectionManager(cm);
-            httpClientBuilder.setSSLSocketFactory(sslsf);
-            final CloseableHttpClient httpClient = httpClientBuilder.build();
-            final ApacheHttpClient4Engine engine = new ApacheHttpClient4Engine(
-                    httpClient, localContext);
 
             // Create and return a client
             final ResteasyClient client = new ResteasyClientBuilder()
                     .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
                     .readTimeout(TIMEOUT, TimeUnit.SECONDS)
-                    .httpEngine(engine).build();
+                    .connectionPoolSize(MAX_CONNECTIONS)
+                    .maxPooledPerRoute(MAX_CONNECTIONS_PER_ROUTE)
+                    .hostnameVerification(HostnameVerificationPolicy.ANY)
+                    .sslContext(builder.build())
+                    .build();
             client.register(new ErrorCaptureResponseFilter());
             return client;
         } catch (NoSuchAlgorithmException | KeyManagementException
@@ -149,56 +112,14 @@ public abstract class RestClientUtils {
     }
 
     /**
-     * Set up a connection context.
-     *
-     * @param url
-     *            Where will the connection be made to?
-     * @param credentials
-     *            What credentials will be used to connect?
-     * @param authScheme
-     *            The authentication scheme to use.
-     * @return the configured context.
-     */
-    private static HttpContextProvider getConnectionContext(final URL url,
-            final Credentials credentials, final AuthScheme authScheme) {
-        int port = url.getPort();
-        if (port == -1) {
-            port = url.getDefaultPort();
-        }
-        HttpHost targetHost = new HttpHost(url.getHost(), port,
-                url.getProtocol());
-
-        CredentialsProvider credsProvider = new BasicCredentialsProvider();
-        credsProvider.setCredentials(
-                new AuthScope(targetHost.getHostName(), targetHost.getPort()),
-                credentials);
-
-        AuthCache authCache = new BasicAuthCache();
-        authCache.put(targetHost, authScheme);
-
-        HttpContext localContext = new BasicHttpContext();
-        localContext.setAttribute(AUTH_CACHE, authCache);
-        localContext.setAttribute(CREDS_PROVIDER, credsProvider);
-        HttpContextProvider provider = new HttpContextProvider() {
-            @Override
-            public HttpContext getContext() {
-                return localContext;
-            }
-        };
-        return provider;
-    }
-
-    /**
      * Create a REST client proxy for a class to the given URL.
      *
      * @param <T>
      *            The type of interface to proxy
      * @param url
      *            The URL of the REST service
-     * @param credentials
-     *            The credentials used to access the service
-     * @param authScheme
-     *            The authentication scheme in use
+     * @param authorizationHeader
+     *            The authorisation header to provide
      * @param clazz
      *            The interface to proxy
      * @param providers
@@ -206,17 +127,24 @@ public abstract class RestClientUtils {
      * @return The proxy instance
      */
     public static <T> T createClient(final URL url,
-            final Credentials credentials, final AuthScheme authScheme,
+            final String authorizationHeader,
             final Class<T> clazz, final Object... providers) {
-        final ResteasyClient client = createRestClient(url, credentials,
-                authScheme);
+        final ResteasyClient client = createRestClient(url);
         for (final Object provider : providers) {
             client.register(provider);
         }
         if (providers.length == 0) {
             client.register(new JacksonJsonProvider());
         }
-        return client.target(url.toString()).proxy(clazz);
+        return client.target(url.toString()).register(
+                new ClientRequestFilter() {
+
+            @Override
+            public void filter(final ClientRequestContext context)
+                    throws IOException {
+                context.getHeaders().add(WWW_AUTH_RESP, authorizationHeader);
+            }
+        }).proxy(clazz);
     }
 
     /**
@@ -239,9 +167,10 @@ public abstract class RestClientUtils {
     public static <T> T createBasicClient(final URL url, final String username,
             final String password, final Class<T> clazz,
             final Object... providers) {
-        return createClient(url,
-                new UsernamePasswordCredentials(username, password),
-                new BasicScheme(), clazz, providers);
+        String userPass = Base64.encodeBase64String(
+                EncodingUtils.getAsciiBytes(username + ":" + password));
+        String authHeader = "Basic " + userPass;
+        return createClient(url, authHeader, clazz, providers);
     }
 
     /**
@@ -264,21 +193,8 @@ public abstract class RestClientUtils {
     public static <T> T createApiKeyClient(final URL url, final String username,
             final String apiKey, final Class<T> clazz,
             final Object... providers) {
-        return createClient(url,
-                new UsernamePasswordCredentials(username, apiKey),
-                new ConnectionIndependentScheme("ApiKey") {
-                    /**
-                     *
-                     */
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    protected Header authenticate(
-                            final Credentials credentials) {
-                        return new BasicHeader(getAuthHeaderName(),
-                                "ApiKey " + username + ":" + apiKey);
-                    }
-                }, clazz, providers);
+        return createClient(url, "ApiKey " + username + ":" + apiKey,
+                clazz, providers);
     }
 
     /**
@@ -298,104 +214,6 @@ public abstract class RestClientUtils {
      */
     public static <T> T createBearerClient(final URL url, final String token,
             final Class<T> clazz, final Object... providers) {
-        return createClient(url, new UsernamePasswordCredentials("", token),
-                new ConnectionIndependentScheme("Bearer") {
-                    /**
-                     *
-                     */
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    protected Header authenticate(
-                            final Credentials credentials) {
-                        return new BasicHeader(getAuthHeaderName(),
-                                "Bearer " + token);
-                    }
-                }, clazz, providers);
-    }
-
-    /**
-     * Base for authorisation schemes.
-     */
-    private abstract static class ConnectionIndependentScheme
-            extends RFC2617Scheme {
-
-        /**
-         *
-         */
-        private static final long serialVersionUID = 1L;
-
-        /**
-         * True when complete.
-         */
-        private final boolean complete = false;
-
-        /**
-         * The name of the scheme.
-         */
-        private final String name;
-
-        /**
-         * Create a new scheme.
-         * @param nameParam The name of the scheme
-         */
-        ConnectionIndependentScheme(final String nameParam) {
-            this.name = nameParam;
-        }
-
-        @Override
-        public String getSchemeName() {
-            return name;
-        }
-
-        @Override
-        public boolean isConnectionBased() {
-            return false;
-        }
-
-        @Override
-        public boolean isComplete() {
-            return complete;
-        }
-
-        /**
-         * Produce an authorisation header for the given set of
-         * {@link Credentials}. The credentials and the connection will have
-         * been sanity-checked prior to this call.
-         *
-         * @param credentials
-         *            The credentials to be authenticated.
-         * @return the header
-         */
-        protected abstract Header authenticate(Credentials credentials);
-
-        /**
-         * Give the header that we're supposed to generate, depending on whether
-         * we're going by a proxy or not.
-         *
-         * @return the authentication header name
-         */
-        protected String getAuthHeaderName() {
-            if (isProxy()) {
-                return PROXY_AUTH_RESP;
-            } else {
-                return WWW_AUTH_RESP;
-            }
-        }
-
-        @Override
-        public Header authenticate(final Credentials credentials,
-                final HttpRequest request) throws AuthenticationException {
-            if (credentials == null) {
-                throw new IllegalArgumentException(
-                        "Credentials may not be null");
-            }
-            if (request == null) {
-                throw new IllegalArgumentException(
-                        "HTTP request may not be null");
-            }
-
-            return authenticate(credentials);
-        }
+        return createClient(url, "Bearer " + token, clazz, providers);
     }
 }
